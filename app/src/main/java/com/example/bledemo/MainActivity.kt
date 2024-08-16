61eadc78f5029ae0caf4e0aa3695937b707cf326
linyingfa <530810890@qq.com>
2024-08-16 16:14:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bledemo/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.bledemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bledemo.ui.theme.BleDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BleDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Column (modifier = modifier){
+        Row {
+            Button(onClick = { /*TODO*/ }) {
+                Text(
+                    text = "扫描",
+                )
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(
+                    text = "停止扫描",
+                )
+            }
+
+        }
+    }
+}
+
